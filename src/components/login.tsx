78fa0f789c6cd9cc8c2a096fa29956db2c84d578
Anthony Shellman <atshellman@gmail.com>
2023-07-14 15:53:04 -0600

    Add placeholder button

--- src/components/login.tsx
+++ src/components/login.tsx
@@ -7,29 +7,32 @@ const Login = () => {
   const [password, setPassword] = useState('');
 
   return (
-    <form className="flex flex-col items-center">
-      <div className="flex flex-col items-center">
-        <label htmlFor="userName">User Name</label>
-        <input 
-          id="userName" 
-          className="border border-black-300"
-          type="text"
-          value={userName}
-          onChange={e => setUserName(e.target.value)}
-        />
-      </div>
-      <div className="flex flex-col items-center">
-        <label htmlFor="password">Password</label>
-        <input 
-          id="password"
-          className="border border-black-300"
-          type="password"
-          value={password}
-          onChange={e => setPassword(e.target.value)}
-        />
-      </div>
-      <input type="submit" value="Sign In" className="border border-black-300"/>
-    </form>
+    <section className="flex flex-col items-center">
+      <form className="flex flex-col items-center">
+        <div className="flex flex-col items-center">
+          <label htmlFor="userName">User Name</label>
+          <input 
+            id="userName" 
+            className="border border-black-300"
+            type="text"
+            value={userName}
+            onChange={e => setUserName(e.target.value)}
+          />
+        </div>
+        <div className="flex flex-col items-center">
+          <label htmlFor="password">Password</label>
+          <input 
+            id="password"
+            className="border border-black-300"
+            type="password"
+            value={password}
+            onChange={e => setPassword(e.target.value)}
+          />
+        </div>
+        <input type="submit" value="Sign In" className="border border-black-300"/>
+      </form>
+      <button className="border border-black-300">Sign Up</button>
+    </section>
   )
 }
 
