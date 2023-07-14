64296569a0d581b7895d08e1bd3fdea782a4a59c
Anthony Shellman <atshellman@gmail.com>
2023-07-14 17:10:47 -0600

    Initial commit

---
+++ src/components/signup.tsx
@@ -0,0 +1,74 @@
+import { useState } from "react"
+
+const SignUp = () => {
+  const [userName, setUserName] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const [selection, setSelection] = useState('');
+  const [email, setEmail] = useState('');
+
+  return (
+    <section className="flex flex-col items-center">
+      <form className="flex flex-col items-center box-border 
+        h-80 w-64 p-4 bg-indigo-500">
+      <div className="flex flex-col items-center">
+          <label htmlFor="userName">User Name</label>
+          <input 
+            id="userName"
+            className="border border-black-300"
+            type="text"
+            value={userName}
+            onChange={e => setUserName(e.target.value)}
+          />
+        </div>
+        <div className="flex flex-col items-center">
+          <label htmlFor="password">Password</label>
+          <input 
+            id="password"
+            className="border border-black-300"
+            type="password"
+            value={password}
+            onChange={e => setPassword(e.target.value)}
+          />
+        </div>
+        <div className="flex flex-col items-center">
+          <label htmlFor="confirmPassword">Confirm Password</label>
+          <input 
+            id="confirm-password"
+            className="border border-black-300"
+            type="password"
+            value={confirmPassword}
+            onChange={e => setConfirmPassword(e.target.value)}
+          />
+        </div>
+        <div className="flex flex-col items-center">
+          <label htmlFor="email">Email</label>
+          <input 
+            id="email"
+            className="border border-black-300"
+            type="password"
+            value={email}
+            onChange={e => setEmail(e.target.value)}
+          />
+        </div>
+        <div className="flex flex-col items-center">
+          <label htmlFor="user-type">User Type</label>
+          <select id="user-type" className="w-44"
+            onChange={e => setSelection(e.target.value)}
+          >
+            <option value="select">Select Type</option>
+            <option value="student">Student</option>
+            <option value="alum">Alum</option>
+          </select>
+        </div>
+        <input 
+          type="submit" 
+          value="Submit" 
+          className="border border-black-300"
+        />
+      </form>
+    </section>
+  )
+}
+
+export default SignUp
