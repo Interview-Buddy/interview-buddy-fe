bd353136ee38e656660f220f9c9e782e591ee443
Natalie Kinsaul <n.kinsaul@gmail.com>
2023-07-14 11:10:43 -0700

    Create control form for user login

--- src/components/login.tsx
+++ src/components/login.tsx
@@ -1,9 +1,35 @@
+import { useState } from "react"
 
 
 const Login = () => {
+
+  const [userName, setUserName] = useState('');
+  const [password, setPassword] = useState('');
+
   return (
-    <div>Hello World</div> 
+    <form className="flex flex-col items-center">
+      <div className="flex flex-col items-center">
+        <label htmlFor="userName">User Name</label>
+        <input 
+          id="userName" 
+          className="border border-black-300"
+          type="text"
+          value={userName}
+          onChange={e => setUserName(e.target.value)}
+        />
+      </div>
+      <div className="flex flex-col items-center">
+        <label htmlFor="password">Password</label>
+        <input 
+          id="password"
+          className="border border-black-300"
+          type="password"
+          value={password}
+          onChange={e => setPassword(e.target.value)}
+        />
+      </div>
+    </form>
   )
 }
 
-export default Login
+export default Login;
