c72dc6faa49c413a136a44cab8a7285b38c19842
Anthony Shellman <atshellman@gmail.com>
2023-07-28 14:32:54 -0600

    Add background color to body

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
   }) {
     return (
       <html lang="en">
-        <body>
+        <body className='bg-[#A9DEF9]'>
             <Header />
             {children}
         </body>
