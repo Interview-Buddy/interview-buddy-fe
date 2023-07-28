66a0484d1954d43081edc901a15fc5a893997345
Anthony Shellman <atshellman@gmail.com>
2023-07-28 12:16:16 -0600

    Update variable and setter names, add position styling

--- components/login.tsx
+++ components/login.tsx
@@ -7,14 +7,18 @@ import SignUp from "./signup";
 const Login = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const [modal, showModal] = useState(false);
+  const [modalShow, setModalShow] = useState(false);
   const router = useRouter();
+
+  const modalHandler = () => {
+    setModalShow(!modalShow)
+  }
 
   return (
     <>
-      {modal && <SignUp />}
+      {modalShow && <SignUp />}
 
-      <section className="flex flex-col items-center">
+      <section className="flex flex-col items-center h-[45rem] place-content-center">
         <form className="flex flex-col items-center" onSubmit={(event) => {event.preventDefault(), router.push('/dashboard')}}>
           <div className="flex flex-col items-center">
             <label htmlFor="userEmail">Email</label>
@@ -44,7 +48,7 @@ const Login = () => {
         </form>
         <button 
           className="border border-black-300"
-          onClick={e => showModal(true)}
+          onClick={e => modalHandler()}
           >Sign Up</button>
       </section>
     </>
