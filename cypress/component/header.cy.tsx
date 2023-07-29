a53cbdcac03bef38960c444f8a7e142a3058e9dc
Anthony Shellman <atshellman@gmail.com>
2023-07-29 13:09:03 -0600

    Add test for welcome banner

--- cypress/component/header.cy.tsx
+++ cypress/component/header.cy.tsx
@@ -2,5 +2,10 @@ import Header from "@/components/header";
 
 
 describe('header component', () => {
-  
+  it('Mounts with a banner message', () => {
+    cy.mount(<Header />)
+    cy.get('[data-cy="header"]')
+    .get('[data-cy="banner"]')
+    .contains('Welcome to Interview Buddy!')
+  })
 })
