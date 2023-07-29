ac0853778df3a1a28bf7813b66aced7a898d3e22
Anthony Shellman <atshellman@gmail.com>
2023-07-29 12:56:46 -0600

    Refactor to include targeting by data-cy properties

--- cypress/component/login.cy.tsx
+++ cypress/component/login.cy.tsx
@@ -5,15 +5,17 @@ import MockNextRouter from "../utils/router";
 describe('login component', () => {
   it('Mounts an input with label for the user\'s email.', () => {
     cy.mount(<MockNextRouter><Login/></MockNextRouter>)
-    cy.get('label').contains('User Name');
+    cy.get('[data-cy="email-label"]').contains('Email')
   })
 
   it('Mounts an input with label for the user\'s password.', () => {
-    // cy.mount()
+    cy.mount(<MockNextRouter><Login/></MockNextRouter>)
+    cy.get('[data-cy="password-label"]').contains('Password');
   })
 
   it('Has a button to submit the user\'s email and password.', () => {
-    // cy.mount()
+    cy.mount(<MockNextRouter><Login/></MockNextRouter>)
+    cy.get('[data-cy="signin"]').should('have.value', 'Sign In');
   })
 
   it('Disables the submit button until the user enters a valid email address.', () => {
