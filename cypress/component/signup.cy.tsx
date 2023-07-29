88ca6474e7f2702001fafc73de2e6dadcf17d8e5
Anthony Shellman <atshellman@gmail.com>
2023-07-29 13:02:00 -0600

    Initial commit

---
+++ cypress/component/signup.cy.tsx
@@ -0,0 +1,7 @@
+import SignUp from "@/components/signup";
+import { useRouter } from "next/navigation";
+import MockNextRouter from "../utils/router";
+
+describe('signup component', () => {
+  
+})
