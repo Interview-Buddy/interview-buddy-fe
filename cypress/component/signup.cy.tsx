fa810149f0ead0705cb03ec944bc9324516b2da7
Anthony Shellman <atshellman@gmail.com>
2023-07-29 16:15:25 -0600

    Add mock function, add additional input form tests

--- cypress/component/signup.cy.tsx
+++ cypress/component/signup.cy.tsx
@@ -1,38 +1,40 @@
 import SignUp from "@/components/signup";
 import { useRouter } from "next/navigation";
 import MockNextRouter from "../utils/router";
-import { useState, MouseEvent } from "react";
-
 
 describe('signup component', () => {
-  const [modalShow, setModalShow] = useState(false);
-  const modalHandler = (e: MouseEvent<HTMLButtonElement>):void => {
-    e.preventDefault()
-    setModalShow(!modalShow)
+  const modalHandler = ():null => {
+    return null
   }
 
   it('Mounts an input with label for the user\'s display name.', () => {
     cy.mount(<MockNextRouter><SignUp modalHandler={modalHandler}/></MockNextRouter>)
     cy.get('[data-cy="display-name-label"]').contains('Display Name')
+    cy.get('[data-cy="display-name"]').type("Display Name")
   })
 
   it('Mounts an input with label for the user\'s email.', () => {
     cy.mount(<MockNextRouter><SignUp modalHandler={modalHandler}/></MockNextRouter>)
     cy.get('[data-cy="email-label"]').contains('Email')
+    cy.get('[data-cy="email"]').type("name@test.com")
   })
 
   it('Mounts an input with label for the user\'s password.', () => {
     cy.mount(<MockNextRouter><SignUp modalHandler={modalHandler}/></MockNextRouter>)
     cy.get('[data-cy="password-label"]').contains('Password')
+    cy.get('[data-cy="password"]').type("SuP3rS3cr3tPassw0rd!")
   })
 
   it('Mounts an input with label for the user\'s password confirmation.', () => {
     cy.mount(<MockNextRouter><SignUp modalHandler={modalHandler}/></MockNextRouter>)
     cy.get('[data-cy="confirm-password-label"]').contains('Confirm Password')
+    cy.get('[data-cy="confirm-password"]').type("SuP3rS3cr3tPassw0rd!")
   })
 
   it('Mounts an input with label for the user\'s user type selection.', () => {
     cy.mount(<MockNextRouter><SignUp modalHandler={modalHandler}/></MockNextRouter>)
     cy.get('[data-cy="user-type-label"]').contains('User Type')
+    cy.get('[data-cy="select"]').select('student')
+    cy.get('[data-cy="select"]').select('alum')
   })
 })
