9896be4523a0e29114d29a1958ee56fe380f0fb5
Anthony Shellman <atshellman@gmail.com>
2023-07-29 16:22:20 -0600

    Add tests for inputs

--- cypress/component/login.cy.tsx
+++ cypress/component/login.cy.tsx
@@ -6,11 +6,13 @@ describe('login component', () => {
   it('Mounts an input with label for the user\'s email.', () => {
     cy.mount(<MockNextRouter><Login/></MockNextRouter>)
     cy.get('[data-cy="email-label"]').contains('Email')
+    cy.get('[data-cy="user-email"]').type("name@test.com")
   })
 
   it('Mounts an input with label for the user\'s password.', () => {
     cy.mount(<MockNextRouter><Login/></MockNextRouter>)
     cy.get('[data-cy="password-label"]').contains('Password');
+    cy.get('[data-cy="user-password"]').type("Op3NSezM3!!")
   })
 
   it('Has a button to submit the user\'s email and password.', () => {
