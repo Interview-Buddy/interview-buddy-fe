0526c01d4b818eae97dac4ffe734a8971dcddc83
Lucas Merchant <lbmerchant93@gmail.com>
2023-07-31 13:05:50 -0600

    Render initial calendar

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,5 +1,9 @@
 "use client";
 import React, { useState } from "react";
+import FullCalendar from '@fullcalendar/react';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import timeGridPlugin from '@fullcalendar/timegrid';
+import interactionPlugin from '@fullcalendar/interaction';
 
 const mockStudent = {
     displayName: "Mock Student",
@@ -12,13 +16,13 @@ const Dashboard = () => {
     const [interviewType, setInterviewType] = useState('');
 
     return (
-        <div className="flex">
+        <div className="flex flex-col md:flex-row">
             <section className="flex flex-col p-4">
                 <h2 className="text-2xl" data-cy="user-displayName">{mockStudent.displayName}</h2>
                 <p data-cy="user-timezone">PST 00:00</p>
-                <div className="flex flex-row">
+                <div className="flex flex-row md:flex-col">
                     <label htmlFor="interview-type" data-cy="interview-type-label">Interview Type:</label>
-                    <select id="interview-type" data-cy="select" className="ml-2 w-44 border-2"
+                    <select id="interview-type" data-cy="select" className="ml-2 md:ml-0 w-35 border-2"
                         onChange={e => setInterviewType(e.target.value)}
                     >
                         <option value="select">Select Type</option>
@@ -26,6 +30,23 @@ const Dashboard = () => {
                         <option value="technical">Technical</option>
                     </select>
                 </div>
+            </section>
+            <section className="p-4 w-full h-full">
+                <FullCalendar
+                    //Used to set the height of the calendar content without scroll
+                    contentHeight={'auto'}
+                    //dayGridPlugin: Month and Day grid views, interactionPlugin: required to detect dateClick actions, selectable actions, and event drag-n-drop & resizing.
+                    plugins={[
+                        dayGridPlugin,
+                        timeGridPlugin,
+                        interactionPlugin
+                    ]}
+                    headerToolbar={{
+                        left: 'prev,next today',
+                        center: 'title',
+                        right: 'dayGridMonth,timeGridWeek,timeGridDay',
+                    }}
+                />
             </section>
         </div>
     );
