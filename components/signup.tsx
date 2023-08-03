d56203777a7f07addd317063d39786e5bfc94fe0
Anthony Shellman <atshellman@gmail.com>
2023-08-03 09:43:00 -0600

    Add margin top for various labels

--- components/signup.tsx
+++ components/signup.tsx
@@ -20,7 +20,7 @@ const SignUp:FC<SignUpProps> = ( { modalHandler } ) => {
   return (
     <section className="flex flex-col items-center absolute inset-0 top-20 h-[40rem] place-content-center">
       <form 
-        className="flex flex-col items-center box-border h-[30rem] w-64 p-4 bg-[#E4C1F9]">
+        className="flex flex-col items-center box-border h-[35rem] w-64 p-4 bg-[#E4C1F9]">
         <div className="flex justify-end w-56">
           <button data-cy="exit-button" onClick={(e) => modalHandler(e)}>X</button>
         </div>
@@ -37,7 +37,7 @@ const SignUp:FC<SignUpProps> = ( { modalHandler } ) => {
           />
         </div>
         <div className="flex flex-col items-center">
-          <label htmlFor="lastName" data-cy="last-name-label">Last Name</label>
+          <label htmlFor="lastName" data-cy="last-name-label" className="mt-2">Last Name</label>
           <input
             id="lastName"
             className="border border-black-300"
@@ -49,7 +49,7 @@ const SignUp:FC<SignUpProps> = ( { modalHandler } ) => {
           />
         </div>
         <div className="flex flex-col items-center">
-          <label htmlFor="displayName" data-cy="display-name-label">Display Name</label>
+          <label htmlFor="displayName" data-cy="display-name-label" className="mt-2">Display Name</label>
           <input
             id="displayName"
             className="border border-black-300"
@@ -61,7 +61,7 @@ const SignUp:FC<SignUpProps> = ( { modalHandler } ) => {
           />
         </div>
         <div className="flex flex-col items-center">
-          <label htmlFor="pronouns" data-cy="pronouns-label">Pronouns</label>
+          <label htmlFor="pronouns" data-cy="pronouns-label" className="mt-2">Pronouns</label>
           <input
             id="pronouns"
             className="border border-black-300"
@@ -73,7 +73,7 @@ const SignUp:FC<SignUpProps> = ( { modalHandler } ) => {
           />
         </div>
         <div className="flex flex-col items-center">
-          <label htmlFor="email" data-cy="email-label">Email</label>
+          <label htmlFor="email" data-cy="email-label" className="mt-2">Email</label>
           <input 
             id="email"
             className="border border-black-300"
@@ -85,7 +85,7 @@ const SignUp:FC<SignUpProps> = ( { modalHandler } ) => {
           />
         </div>
         <div className="flex flex-col items-center">
-          <label htmlFor="password" data-cy="password-label">Password</label>
+          <label htmlFor="password" data-cy="password-label" className="mt-2">Password</label>
           <input 
             id="password"
             className="border border-black-300"
@@ -97,7 +97,7 @@ const SignUp:FC<SignUpProps> = ( { modalHandler } ) => {
           />
         </div>
         <div className="flex flex-col items-center">
-          <label htmlFor="confirmPassword" data-cy="confirm-password-label">Confirm Password</label>
+          <label htmlFor="confirmPassword" data-cy="confirm-password-label" className="mt-2">Confirm Password</label>
           <input 
             id="confirmPassword"
             className="border border-black-300"
@@ -109,7 +109,7 @@ const SignUp:FC<SignUpProps> = ( { modalHandler } ) => {
           />
         </div>
         <div className="flex flex-col items-center">
-          <label htmlFor="user-type" data-cy="user-type-label">User Type</label>
+          <label htmlFor="user-type" data-cy="user-type-label" className="mt-2">User Type</label>
           <select id="user-type" data-cy="select" className="w-44"
             onChange={e => setSelection(e.target.value)}
           >
