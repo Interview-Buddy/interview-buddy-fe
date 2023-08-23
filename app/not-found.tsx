2f422e227999f0da5fe03edb0cc20af270f59d6c
Lucas Merchant <lbmerchant93@gmail.com>
2023-08-23 14:47:37 -0600

    Add initial test for NotFound URL

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -4,9 +4,9 @@ const NotFound = () => {
   return (
     <main className="text-center max-w-screen-xl flex justify-center mt-10">
         <div className="bg-[#E4C1F9] w-72 p-4">
-            <h2 className="text-2xl">There was a problem.</h2>
-            <p>Sorry! We could not find the page you were looking for.</p>
-            <p>Go back to the <Link href="/" className="underline text-sky-400">Home Page</Link></p>
+            <h2 className="text-2xl" data-cy="error-message">There was a problem.</h2>
+            <p data-cy="error-message-description">Sorry! We could not find the page you were looking for.</p>
+            <p>Go back to the <Link href="/" className="underline text-sky-400" data-cy="redirect-link">Home Page</Link></p>
         </div>
     </main>
   );
