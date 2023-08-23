36d7fa7bd5b76cc58d5bc1f4b651639e0f1f24b6
Lucas Merchant <lbmerchant93@gmail.com>
2023-08-23 14:47:37 -0600

    Add initial test for NotFound URL

---
+++ cypress/e2e/NotFound.cy.ts
@@ -0,0 +1,12 @@
+describe('Not Found URL', () => {
+    // It looks like this reloads the NotFound page like it does in development, so I wonder if we need to be mindful of that while testing this page. If you comment out the clicking of the Link the behavior is demonstrated.
+    // I believe we also will need to stub the initial useUser query in AuthProvider because it calls each time the app is Rendered.
+    it('Displays an error message when an invalid url path is entered and a link to redirect the user.', () => {
+        cy.request({url: '/404', failOnStatusCode: false}).its('status').should('equal', 404);
+        cy.visit('/404', {failOnStatusCode: false});
+        cy.get('[data-cy="error-message"]').contains('There was a problem.');
+        cy.get('[data-cy="error-message-description"]').contains('Sorry! We could not find the page you were looking for.');
+        cy.get('[data-cy="redirect-link"]').click();
+        cy.url().should('eq', 'http://localhost:3000/');
+    });
+});
