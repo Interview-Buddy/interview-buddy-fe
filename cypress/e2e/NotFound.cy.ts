679dd9129f2e93b2030f15b127f411b8def28739
Lucas Merchant <lbmerchant93@gmail.com>
2023-08-23 15:04:42 -0600

    Start trying to stub useUser query. Incomplete.

--- cypress/e2e/NotFound.cy.ts
+++ cypress/e2e/NotFound.cy.ts
@@ -1,6 +1,32 @@
+import { hasOperationName, aliasQuery } from '../utils/graphql-test-utils';
+
 describe('Not Found URL', () => {
     // It looks like this reloads the NotFound page like it does in development, so I wonder if we need to be mindful of that while testing this page. If you comment out the clicking of the Link the behavior is demonstrated.
+
     // I believe we also will need to stub the initial useUser query in AuthProvider because it calls each time the app is Rendered.
+    beforeEach(() => {
+        cy.intercept('POST', "https://interview-buddy-be.onrender.com/graphql", (req) => {
+            //Queries
+            aliasQuery(req, 'User');
+
+            if (hasOperationName(req, 'User')) {
+                req.alias = 'gqlUserQuery'
+        
+                req.reply((res) => {
+                    res.body.data.company = "Bob's Burgers"
+                    res.body.data.user.displayName = "Bob Belcher"
+                    res.body.data.user.email = "bob@burgers.com"
+                    res.body.data.user.firstName = "Bob"
+                    res.body.data.user.id = "1"
+                    res.body.data.user.lastName = "Belcher"
+                    res.body.data.user.pronouns = "he/him"
+                    res.body.data.user.userType = 0
+                    res.body.data.user.uuid = "1a1a1a"
+                })
+            }
+        });
+    });
+
     it('Displays an error message when an invalid url path is entered and a link to redirect the user.', () => {
         cy.request({url: '/404', failOnStatusCode: false}).its('status').should('equal', 404);
         cy.visit('/404', {failOnStatusCode: false});
