06866486e51a4fcf578d6f55d9d61b9a7a7ddd8c
Anthony Shellman <atshellman@gmail.com>
2023-08-29 16:01:05 -0600

    Remove auth context and related setter functions

--- components/LogOutButton.tsx
+++ components/LogOutButton.tsx
@@ -2,26 +2,15 @@ import { MouseEvent, useContext } from "react"
 import { signOut } from "firebase/auth"
 import { auth } from "../configs/firebase.configs"
 import { FirebaseError } from "firebase/app"
-import { AuthContext } from "../app/auth-provider";
 import { useRouter } from 'next/navigation'
 
 const LogOutButton = () => {
   const router = useRouter();
-  const user = useContext(AuthContext);
 
   const logout = async (e: MouseEvent<HTMLButtonElement>) => {
     e.preventDefault();
     try {
       await signOut(auth)
-      user.setCompany(null)
-      user.setDisplayName(null)
-      user.setEmail(null)
-      user.setFirstName(null)
-      user.setIsLoggedIn(false)
-      user.setLastName(null)
-      user.setPronouns(null)
-      user.setUserType(null)
-      user.setUuid(undefined)
       router.push('/');
     } catch (err: unknown) {
       const errorMessage = err as FirebaseError
