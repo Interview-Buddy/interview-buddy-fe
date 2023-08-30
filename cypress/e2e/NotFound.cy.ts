4c3e19b38f8445dd5391b3f1034a83f43b616929
Lucas Merchant <lbmerchant93@gmail.com>
2023-08-30 13:22:33 -0600

    Remove beforeEach stubbing of User query, no longer hard coded so isnt enabled

--- cypress/e2e/NotFound.cy.ts
+++ cypress/e2e/NotFound.cy.ts
@@ -3,35 +3,6 @@ import { hasOperationName, aliasQuery } from '../utils/graphql-test-utils';
 describe('Not Found URL', () => {
     // It looks like this reloads the NotFound page like it does in development, so I wonder if we need to be mindful of that while testing this page. If you comment out the clicking of the Link the behavior is demonstrated.
 
-    // I believe we also will need to stub the initial useUser query in AuthProvider because it calls each time the app is Rendered.
-    beforeEach(() => {
-        cy.intercept('POST', "https://interview-buddy-be.onrender.com/graphql*", (req) => {
-            if (hasOperationName(req, 'user')) {
-                req.reply(
-                    {
-                        statusCode: 200,
-                        body: {
-                            data: {
-                                user: {
-                                    company: "Bob's Burgers",
-                                    displayName: "Bob Belch",
-                                    email: "bob@burgers.com",
-                                    firstName: "Bob",
-                                    lastName: "Belcher",
-                                    pronouns: "he/him",
-                                    userType: 0,
-                                    uuid: "1a1a1a"
-                                }
-                            }
-                        }
-                    }
-                )
-            }
-        }).as("gqluserQuery");
-        cy.visit('/');
-        cy.wait('@gqluserQuery');
-    });
-
     it('Displays an error message when an invalid url path is entered and a link to redirect the user.', () => {
         cy.request({url: '/404', failOnStatusCode: false}).its('status').should('equal', 404);
         cy.visit('/404', {failOnStatusCode: false});
