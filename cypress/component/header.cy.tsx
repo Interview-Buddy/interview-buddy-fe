acf3ee91427320c8cdd44c8390c97f765a6c9a32
Lucas Merchant <lbmerchant93@gmail.com>
2023-09-01 12:39:05 -0600

    Rename Header file and Header test file, update imports of Header location. Write out initial tests for new Header.

--- cypress/component/header.cy.tsx
+++ cypress/component/header.cy.tsx
@@ -1,11 +1,23 @@
-import Header from "@components/header";
+import Header from "@components/Header";
 
+describe('Header component', () => {
+  it('Mounts a Header tag.', () => {
+    cy.mount(<Header />);
+  });
 
-describe('header component', () => {
-  it('Mounts with a banner message', () => {
-    cy.mount(<Header />)
-    cy.get('[data-cy="header"]')
-    .get('[data-cy="banner"]')
-    .contains('Welcome to Interview Buddy!')
-  })
-})
+  it.skip('Contains an h1 tag that is the title of the application, this title is also a link to the root URL.', () => {
+    cy.mount(<Header />);
+  });
+
+  it.skip('Contains an img tag that is the application logo with a proper alt attribute for accessibility.', () => {
+    cy.mount(<Header />);
+  });
+
+  it.skip('Does not display a Logout Button when a user is not logged in.', () => {
+    cy.mount(<Header />);
+  });
+
+  it.skip('Displays a Logout Button when a user is logged in.', () => {
+    cy.mount(<Header />);
+  });
+});
