6dcef77dbc6184cd8c04c4570137ba245ab2b61b
Anthony Shellman <atshellman@gmail.com>
2023-09-11 19:33:28 -0600

    Add pseudo tests for Login and Signup E2E

--- cypress/e2e/home.cy.ts
+++ cypress/e2e/home.cy.ts
@@ -11,4 +11,14 @@ describe('Home page', () => {
   it('Visits baseUrl, which contains a button to sign up.', () => {
     cy.visit('/')
   });
+
+  it.skip('Should be able to Sign In', () => {
+    // Need to stub/mock and intercept GraphQL query
+    // "Sign In" button value changes to "Loading ..." as part of this action (subject to change)
+  })
+
+  it.skip('Should be able to Sign Up', () => {
+    // Need to stub/mock and intercept GraphQL mutation
+    // "Submit" button value changes to "Loading ..." as part of this action (subject to change)
+  })
 });
